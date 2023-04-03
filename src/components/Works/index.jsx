d864d5c9a2ca8a8a920dfd72b34f3a363777cfe2
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-04-03 22:04:49 +0500

    2 files modified

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import Svayper from '../Works/swiper';
 
 function Works() {
+  const datas = [
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fidealcleaning.f150a8b4.jpg&w=640&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffayzbaraka.4b28ceb6.jpg&w=640&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Findebt-1.4e4a01c7.jpg&w=640&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimbastudio.f88889bf.jpg&w=1200&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Findebt.a68271f0.jpg&w=1200&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsuvtaminoti.e6d0c4ba.jpg&w=1200&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Finreport.7f9ad8ab.jpg&w=1200&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flazydev.99b90e27.jpg&w=1200&q=75',
+    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmamirovgroup.f46234cc.jpg&w=1200&q=75',
+  ];
   return (
     <section className="container mx-auto py-10 md:py-14 lg:py-16 xl:py-20 !px-[5rem] ">
       <div
@@ -18,7 +29,7 @@ function Works() {
         </div>
       </div>
       <div className="swiper swiper-initialized swiper-horizontal swiper-pointer-events my-swiper">
-        <Svayper />
+        <Svayper datas={datas} />
       </div>
     </section>
   );
--- src/components/Works/swiper.jsx
+++ src/components/Works/swiper.jsx
@@ -12,18 +12,7 @@ import './works.css';
 // import required modules
 import { Autoplay } from 'swiper';
 
-function Svayper() {
-  const datas = [
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fidealcleaning.f150a8b4.jpg&w=640&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffayzbaraka.4b28ceb6.jpg&w=640&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Findebt-1.4e4a01c7.jpg&w=640&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimbastudio.f88889bf.jpg&w=1200&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Findebt.a68271f0.jpg&w=1200&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsuvtaminoti.e6d0c4ba.jpg&w=1200&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Finreport.7f9ad8ab.jpg&w=1200&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flazydev.99b90e27.jpg&w=1200&q=75',
-    'https://www.dotsoft.uz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmamirovgroup.f46234cc.jpg&w=1200&q=75',
-  ];
+function Svayper({ datas }) {
   return (
     <>
       <Swiper
